5aaed9e53ed330006be3d0220361c41663e75550
yogeshpandey009 <yogeshpandey009@gmail.com>
2016-04-27 17:16:02 -0700

    deleting old project files

--- MyCompiler/src/main/java/edu/asu/runtime/Instruction.java
+++
@@ -1,5 +0,0 @@
-package edu.asu.runtime;
-
-public enum Instruction {
-	HALT, PUSH, ADD, SUB, MUL, DIV, MOD, POW, NOT, AND, OR, POP, ISEQ, ISGE, ISGT, JIF, JMP, LOAD, STORE, CALL, RET, LABEL, PRINT, INPUT, STACK_PUSH, STACK_POP, STACK_PEEK, STACK_ISEMPTY
-}
--- MyCompiler/src/main/java/edu/asu/runtime/exceptions/ProgramExecutionException.java
+++
@@ -1,7 +0,0 @@
-package edu.asu.runtime.exceptions;
-
-public class ProgramExecutionException extends RuntimeException {
-    public ProgramExecutionException(String message) {
-        super(message);
-    }
-}
